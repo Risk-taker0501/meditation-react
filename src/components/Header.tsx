8092f4d95b08b51b445b2f0bb404cd100c859b1b
Risk-taker0501 <risk.taker2024@gmail.com>
2023-08-29 19:41:05 +0300

    fix repo name url3

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,13 +10,13 @@ const Header = () => {
       <div className="container max-w-[1140px] mx-auto h-[90px] flex items-center justify-between">
         <Logo />
         <ul className="h-full flex items-center">
-          <Link className="h-full" to={"/#"}>
+          <Link className="h-full" to={"/meditation-react/#"}>
             <Dropdown name="Subscribe" />
           </Link>
-          <Link className="h-full" to={"/about"}>
+          <Link className="h-full" to={"/meditation-react/about"}>
             <Dropdown name="About" />
           </Link>
-          <Link className="h-full" to={"/merch"}>
+          <Link className="h-full" to={"/meditation-react/merch"}>
             <Dropdown name="Merch" />
           </Link>
         </ul>
